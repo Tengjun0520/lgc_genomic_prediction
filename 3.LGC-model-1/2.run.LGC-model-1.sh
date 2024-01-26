138734fc9e08d5519e17992979f1f4f9b2962db4
Tengjun0520 <67666569+Tengjun0520@users.noreply.github.com>
2024-01-26 14:51:01 +0800

    Update 2.run.LGC-model-1.sh

--- 3.LGC-model-1/2.run.LGC-model-1.sh
+++ 3.LGC-model-1/2.run.LGC-model-1.sh
@@ -4,7 +4,7 @@ gmat2 \
     --mvlmm \
     --mgrm sig.agrm non_sig.agrm \
     --data ../0.example_data/all.pheno \
-	--random id id \
+    --random id id \
     --trait tr1 tr2 \
     --out tr1_tr2 \
     --threads 30 \
